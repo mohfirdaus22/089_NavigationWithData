6c511e3f5ff6d6c8dd6e6876ff80e025b1355a32
mohfirdaus22 <thepashter6@gmail.com>
2023-11-03 15:14:35 +0700

    Add HalamanSatu File

--- app/src/main/java/com/example/praktikum6/HalamanSatu.kt
+++ app/src/main/java/com/example/praktikum6/HalamanSatu.kt
@@ -1,2 +1,55 @@
 package com.example.praktikum6
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.RadioButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+
+@Composable
+fun HalamanSatu(
+    pilihanRasa: List<String>,
+    onSelectionChanged:(String)->   Unit,
+    onConfirmButtonClicked: (Int)-> Unit,
+    onNextButtonClicked:()->    Unit,
+    onCncelButtonClicked:()-> Unit,
+    modifier: Modifier = Modifier
+){
+    var rasaYgDipilih by rememberSaveable { mutableStateOf("")}
+    var textJmlBeli by remember{ mutableStateOf("") }
+
+    Column(modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween){
+        Column(
+            modifier = Modifier
+                .padding(dimensionResource(R.dimen.padding_medium))) {
+            pilihanRasa.forEach { item->
+                Row (modifier = Modifier.selectable(
+                    selected = rasaYgDipilih == item,
+                    onClick = {
+                        rasaYgDipilih = item
+                        onSelectionChanged(item)
+                    }
+                ),
+                    verticalAlignment = Alignment.CenterVertically){
+                    RadioButton(selected = rasaYgDipilih == item,
+                        onClick = {
+                            rasaYgDipilih = item
+                            onSelectionChanged(item)
+                        })
+                }
+            }
+
+        }
+    }
+}
