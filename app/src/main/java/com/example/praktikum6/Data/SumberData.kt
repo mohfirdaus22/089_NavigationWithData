13276c598b0aab2853bc3b548f4357563adbf91d
mohfirdaus22 <thepashter6@gmail.com>
2023-11-03 14:39:54 +0700

    Add DataClass OrderUIState Add DataObject SumberData Add File kotlin FormatUI Add dimens (values) Add String

---
+++ app/src/main/java/com/example/praktikum6/Data/SumberData.kt
@@ -0,0 +1,14 @@
+package com.example.praktikum6.Data
+
+import com.example.praktikum6.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche
+
+    )
+}
