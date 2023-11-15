74641ecae93427132608c453894a1112a3c3475b
mohfirdaus22 <thepashter6@gmail.com>
2023-11-15 21:24:26 +0700

    Add Fun Halaman Form

--- app/src/main/java/com/example/praktikum6/HalamanForm.kt
+++ app/src/main/java/com/example/praktikum6/HalamanForm.kt
@@ -1,2 +1,25 @@
 package com.example.praktikum6
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+
+@Composable
+fun HalamanForm(
+    onSubmitButtonClick:(MutableList<String>) -> Unit
+) {
+    var namaTxt by rememberSaveable {
+        mutableStateOf("")
+    }
+    var alamatTxt by remember {
+        mutableStateOf("")
+    }
+    var telponTxt by remember {
+        mutableStateOf("")
+    }
+    var listDataTxt: MutableList<String> = mutableListOf(namaTxt, alamatTxt, telponTxt)
+
+}
