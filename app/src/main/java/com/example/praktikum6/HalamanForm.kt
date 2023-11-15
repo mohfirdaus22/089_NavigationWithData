3262e0fe6411c7af39b02a7c4ca4f2f3d38662f1
mohfirdaus22 <thepashter6@gmail.com>
2023-11-15 21:32:27 +0700

    Add Spacer and Row for button Submit

--- app/src/main/java/com/example/praktikum6/HalamanForm.kt
+++ app/src/main/java/com/example/praktikum6/HalamanForm.kt
@@ -2,8 +2,11 @@ package com.example.praktikum6
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -15,6 +18,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,7 +26,8 @@ import androidx.compose.ui.unit.sp
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HalamanForm(
-    onSubmitButtonClick:(MutableList<String>) -> Unit
+    onSubmitButtonClick:(MutableList<String>) -> Unit,
+    onCancelButtonClick: () -> Unit
 ) {
     var namaTxt by rememberSaveable {
         mutableStateOf("")
@@ -63,5 +68,15 @@ fun HalamanForm(
         }, label = {
             Text(text = "Masukkan No Telephone")
         })
+        Row{
+            Button(onClick = { onCancelButtonClick() }) {
+                Text(text = "Batal")
+            }
+            Spacer(modifier = Modifier.padding(35.dp))
+
+            Button(onClick =  { onSubmitButtonClick(listDataTxt) }) {
+                Text(text = stringResource(R.string.btn_submit))
+            }
+        }
     }
 }
