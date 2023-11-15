73c58143e778e0c5bd6dfd798bd2d77076c4544f
mohfirdaus22 <thepashter6@gmail.com>
2023-11-15 21:25:50 +0700

    Add Column HalamanForm

--- app/src/main/java/com/example/praktikum6/HalamanForm.kt
+++ app/src/main/java/com/example/praktikum6/HalamanForm.kt
@@ -1,11 +1,18 @@
 package com.example.praktikum6
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HalamanForm(
@@ -22,4 +29,13 @@ fun HalamanForm(
     }
     var listDataTxt: MutableList<String> = mutableListOf(namaTxt, alamatTxt, telponTxt)
 
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ){
+
+    }
 }
